e806006dbc5d08c346b3ebd3fcbbd33a2879c77a
Fredy Nurman Saleh <email@fredyns.net>
2025-09-23 14:07:46 +0700

    basic deployment script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+# laravel deploy scripts
+
+# reset files to  latest git commit
+git reset --hard HEAD
+git pull
+
+# update dependencies
+composer update
+npm update
+
+# first time setup
+php artisan storage:link
+php artisan key:generate
+
+# database
+php artisan migrate:status
+php artisan migrate
+# php artisan migrate:fresh --seed # --force
+
+# tune up
+composer dump-autoload -o
+php artisan optimize:clear
+php artisan config:clear
+php artisan cache:clear
+php artisan event:cache
+#php artisan icons:cache
+npm run build
+
+# supervisor
+#supervisorctl status
+#supervisorctl reread
+#supervisorctl update
+#supervisorctl restart all
+#supervisorctl status
