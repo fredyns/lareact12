14c65238100659c9b22d4ee801f8581bed3b7057
Fredy Nurman Saleh <email@fredyns.net>
2025-09-25 17:01:51 +0700

    add missing types

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -5,6 +5,24 @@ export interface User {
   id: string;
   name: string;
   email: string;
+  avatar?: string;
+  email_verified_at: string | null;
+  two_factor_enabled?: boolean;
+  created_at: string;
+  updated_at: string;
+  [key: string]: unknown;
+}
+
+export interface Auth {
+  user: User;
+}
+
+export interface SharedData {
+  name: string;
+  quote: { message: string; author: string };
+  auth: Auth;
+  sidebarOpen: boolean;
+  [key: string]: unknown;
 }
 
 export interface NavItem {
