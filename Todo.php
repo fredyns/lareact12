e8f9a9f3fe72945bce7c15d461c11003507eef0e
Fredy Nurman Saleh <email@fredyns.net>
2025-09-27 14:41:50 +0700

    set todo

---
+++ Todo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Todo:
+ *  1. in Items\Create > field 'color': click on input to show color picker
+ *  2. in Items\Create > field 'integer':
+ *      - display input field on top slider
+ *      - remove value label
+ *  3. in Items\Create > field 'status', use radio button group instead of select
+ *  4. in Items\Create > field 'longitude' & latitude: keep 3 digit after decimal point
+ *  5. in Items\Create > geo location picker: make it square
+ *  5. in Items\Create > field 'file' & 'image':
+ *      - implement minio
+ *      - upload not working
+ *  6. in Items\Create > field 'markdown_text': preview incorrect
+ * 
+ * 
+ */ 
