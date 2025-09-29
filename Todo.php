9ee4809d3b215d4eaceac136b142a684381086e1
Fredy Nurman Saleh <email@fredyns.net>
2025-09-29 14:26:02 +0700

    set todo

--- Todo.php
+++ Todo.php
@@ -2,6 +2,9 @@
 /**
  * Todo:
  *  6. in Items\Create > field 'markdown_text': preview incorrect
+ *  7. implement upload pool
+ *  8. install laravel boost
+ *  9. review CRUD codebase
  * 
  * 
  */ 
