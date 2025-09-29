a549155ac58bcf21bf97c03c8c7e5d58cd381894
Fredy Nurman Saleh <email@fredyns.net>
2025-09-29 14:42:06 +0700

    more todo

--- Todo.php
+++ Todo.php
@@ -4,6 +4,11 @@
  *  7. implement upload pool
  *  8. install laravel boost
  *  9. review CRUD codebase
- * 
+ *  10. create Items\Form for create & edit
+ *  11. create field 'upload_path' for sample_items
+ *  12. implements 'upload_path' in Items\Create
+ *  13. image preview in Items\Show
+ *  14. pdf preview in Items\Show
+ *  15. 
  * 
  */ 
