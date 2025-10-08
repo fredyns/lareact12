8b3a22b1fa3372bb1bd0700766bcbe9ad4cbb508
Fredy Nurman Saleh <email@fredyns.net>
2025-10-08 09:31:26 +0700

    set todo

--- Todo.php
+++ Todo.php
@@ -2,6 +2,13 @@
 /**
  * Todo:
  *  9. review CRUD codebase
- *  15. 
+ *  15. implement Searchable trait to models & controllers
+ *  16. add property 'disable' to shorty components
+ *  17. implement shorty components to other pages
+ *  18. flatten cards layout in items pages
+ *  19. change AuthenticatedLayout to AppLayout in RBAC pages
+ *  20. set $keyType = 'string'; in model Role & Permission
+ *  21. add prefix 'rbac_' to Role & Permission table names
+ *  22. 
  * 
  */ 
