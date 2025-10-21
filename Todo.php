c572ab02602d8c866bf98f6ed72d9a006b1a6222
Fredy Nurman Saleh <email@fredyns.net>
2025-10-21 16:41:34 +0700

    set todo

--- Todo.php
+++ Todo.php
@@ -1,6 +1,19 @@
 <?php
 /**
  * Todo:
- *  24.
+ *  24. @ show-modal for sub-item:  show dialog title persistently, without shimmering effect.
+ *  25. @ edit-modal for sub-item: show dialog title persistently, without shimmering effect.
+ *  26. @ index-section for sub-items: limit selected columns to save memory
+ *  27. @ show-modal for sub-item: pass only `subItem` object instead of `id`. use optimistic to show fields.
+ *  28. @ edit-modal for sub-item: pass only `subItem` object instead of `id`. use optimistic to edit fields.
+ *  29. @ show item page: create card on top for fields: string, integer, text. convert other cards as tabs.
+ *  30. @ form-fields for items: follow show item layout for placing fields.
+ *  31. create table `sample_tree_nodes` as sample for node tree/nested set application. this table belong to `sample_items`. fields are: id, item_id, parent_id, title, user_id, description & nested set attributes. implement `lazychaser/laravel-nestedset`.
+ *  32. create index section for `sample_tree_nodes` placed in show item page. organize rows as hierarchy.
+ *  33. create 'create-modal' for `sample_tree_nodes` placed in show item page.
+ *  34. create 'show-modal' for `sample_tree_nodes` placed in show item page.
+ *  35. create 'edit-modal' for `sample_tree_nodes` placed in show item page.
+ *  36. sorting `sample_tree_nodes` function in index section with drag & drop.
+ *  37.
  * 
  */ 
