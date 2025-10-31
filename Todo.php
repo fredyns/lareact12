1d6ec9bb3133afdbdbfbc5831b25ecae76486f89
Fredy Nurman Saleh <email@fredyns.net>
2025-10-31 08:21:02 +0700

    done

--- Todo.php
+++ Todo.php
@@ -11,7 +11,6 @@
  *  40. logs for `login`
  *  41. create image thumbnail with `intervention/image`
  *  42. implement next performance optimization @ `documentation/PERFORMANCE_RECOMMENDATIONS_NEXT.md`
- *  43. create notification system
  *  44.
  * 
  */ 
